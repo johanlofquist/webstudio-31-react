c098526252f02f642698bb80f302b99e27f381c4
Johan Löfquist <johan.lofquist@gmail.com>
2024-03-24 21:27:51 +0100

    Added navbar in desktop

---
+++ src/components/Nav.tsx
@@ -0,0 +1,31 @@
+import logo from "../assets/logo.svg";
+
+export const Nav = () => {
+  return (
+    <div className="h-[70px] flex justify-between items-center">
+      <div className="flex items-center gap-2">
+        <img src={logo} className="h-[40px]" />
+        <p className="font-[600] text-[24px]">
+          Webstudio 31<span className="text-[--ws31red]">.</span>
+        </p>
+      </div>
+      <div className="flex gap-5">
+        <a href="" className="py-2 px-4 hover:bg-[--ws31red] hover:text-white">
+          Hem
+        </a>
+        <a href="" className="py-2 px-4 hover:bg-[--ws31red] hover:text-white">
+          Case
+        </a>
+        <a href="" className="py-2 px-4 hover:bg-[--ws31red] hover:text-white">
+          Tjänster
+        </a>
+        <a href="" className="py-2 px-4 hover:bg-[--ws31red] hover:text-white">
+          Om
+        </a>
+        <a href="" className="py-2 px-4 hover:bg-[--ws31red] hover:text-white">
+          Kontakt
+        </a>
+      </div>
+    </div>
+  );
+};
